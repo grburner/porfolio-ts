9ecddea3e58779403823d429d7238b6a74dff6de
Gary Burner <grburner@gmail.com>
2020-11-24 08:47:44 -0500

    committing project descirition

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -1,17 +1,20 @@
 import React from "react";
 import "./style.css";
+import CollapsibleContent from '../CollapsibleContact';
 
 const NavBar: React.FC = () => {
+
   return (
     <div className={'nav-wrapper'}>
       <nav className={'navbar'}>
-        <div></div>
+        <div/>
         <ul>
-          <li><p className={'navbar-item'}>About</p></li>
-          <li><p className={'navbar-item'}>Projects</p></li>
-          <li><p className={'navbar-item'}>Contact</p></li>
+          <li><p className={'navbar-item'}>ABOUT</p></li>
+          <li><p className={'navbar-item'}>PROJECTS</p></li>
+          <li><p className={'navbar-item'}>CONTACT</p></li>
         </ul>
       </nav>
+      {/* <CollapsibleContent /> */}
     </div>
   );
 };
